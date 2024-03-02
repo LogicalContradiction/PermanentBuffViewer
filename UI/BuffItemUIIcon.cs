171f8828889173f4b6f9fb6c0ff6d53f076c3983
Logical Contradiction <chrisgiaramita@yahoo.com>
2024-03-01 23:22:32 -0500

    added documentation

--- UI/BuffItemUIIcon.cs
+++ UI/BuffItemUIIcon.cs
@@ -12,6 +12,15 @@ namespace PermanentBuffViewer.UI
     /// </summary>
     internal class BuffItemUIIcon : BuffItemUIElement
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="item">The item this element will be displaying.</param>
+        /// <param name="usedItem">The condition for showing the full sprite. The sprite will be a silhouette until the item has been used.</param>
+        /// <param name="itemUsedHoverTextKey">Localization key for the text displayed when an item has been used.</param>
+        /// <param name="itemNotUsedHoverTextKey">Localization key for the text displayed when an item has not yet been used.</param>
+        /// <param name="amountIncreaseByKey">Localization key describing the amount the stat is increased by.</param>
+        /// <param name="statIncrasedKey">Localization key describing the player stat that is increased.</param>
         public BuffItemUIIcon(Item item, Condition usedItem, string itemUsedHoverTextKey, 
             string itemNotUsedHoverTextKey, string amountIncreaseByKey, string statIncrasedKey) : 
             base(item, usedItem, itemUsedHoverTextKey, 
